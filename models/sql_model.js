6ea635b809116fcb1a888696c24a5259f74d060a
laukikN <laukiknirgudkar@gmail.com>
2014-04-04 08:25:02 +0530

    removed all callbacks

--- models/sql_model.js
+++ models/sql_model.js
@@ -1,8 +1,8 @@
 var result_sql = {
-	set_name : "result:"
+	set_name : "result_"
 };
 
-module.exports.create_result_table = function ( db, Qid, callback) {
+module.exports.create_result_table = function ( db, Qid) {
 	/*
 		Init for result data to be
 		inserted for Quiz Creator 
@@ -10,37 +10,40 @@ module.exports.create_result_table = function ( db, Qid, callback) {
 	db.query("create table " + result_sql.set_name + Qid + "(user varchar(30) primary key, username varchar(30), total varchar(5));", function  ( err, status) {
 		if( !err ){
 			/* success in inserting data*/
-			callback( null, 1);
+			//callback( null, 1);
+			console.log( status + 'for Qid :::' + Qid );
 		}else{
 			console.log("ERR AT create_result_table INSIDE sql_model.js");
-			callback( 1, null);
+			//callback( 1, null);
 		}
 	});
 }
 
 
-function add_section_coloumn( db, Qid, section_name, callback) {
+function add_section_coloumn( db, Qid, section_name) {
 	db.query("alter table " + result_sql.set_name + Qid + " add " + section_name + " varchar(25);", function ( err, status){
 		if( !err ){
 			/* success in updating table*/
-			callback( null, 1);
+			//callback( null, 1);
+			console.log( status + 'of add for Qid, section_name' + Qid + section_name );
 		}else{
 			console.log("ERR AT add_section_coloumn INSIDE sql_model.js");
-			callback( 1, null);
+			//callback( 1, null);
 		}
 	});
 }
 
 module.exports.add_section_coloumn =  add_section_coloumn;
 
-function drop_section_coloumn( db, Qid, section_name ,callback) {
+function drop_section_coloumn( db, Qid, section_name) {
 	db.query("alter table " + result_sql.set_name + Qid + " drop " + section_name + ";", function ( err, status){
 		if( !err ){
 			/* success in updating table*/
-			callback( null, 1);
+			//callback( null, 1);
+			console.log( status + 'of drop for Qid, section_name' + Qid + section_name );
 		}else{
 			console.log("ERR AT drop_section_coloumn INSIDE sql_model.js");
-			callback( 1, null);
+			//callback( 1, null);
 		}
 	});
 }
@@ -48,22 +51,26 @@ function drop_section_coloumn( db, Qid, section_name ,callback) {
 module.exports.drop_section_coloumn =  drop_section_coloumn;
 
 
-module.exports.insert_data =  function( db, Qid, user, username, marks, callback) {
-	var str = user + username + ","; 
+module.exports.insert_data =  function( db, Qid, user, username, marks) {
+	var str = '"' + user  + '"' +','+ '"' + username + '"' + ','; 
 	var temp = "";
 	var total = 0;
 	for( var i = 0; i < marks.length; i++){
 		temp += marks[i]+",";
 		total += parseInt( marks[i]); 
 	}
-	str += str + total + "," + temp.slice( 0 , -1);
-	db.query("insert into table " + result_sql.set_name + Qid + " values(" + str + ");", function ( err, status){
+	str += total + "," + temp.slice( 0 , -1);
+	console.log( str);
+	var query = "insert into " + result_sql.set_name + Qid + " values(" + str + ");";
+	console.log( query);
+	db.query(query, function ( err, status){
 		if( !err ){
 			/* success in updating table*/
-			callback( null, 1);
+			console.log( status + ' for Qid, user' + Qid + " " + user);
+			//callback( null, 1);
 		}else{
 			console.log("ERR AT insert_data INSIDE sql_model.js");
-			callback( 1, null);
+			//callback( 1, null);
 		}
 	});
 }
